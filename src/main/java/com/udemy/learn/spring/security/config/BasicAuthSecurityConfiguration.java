c78983a078cad378b40f0ae2c47265ff7489baff
Harkiran Mahun <harkiransinghmahun@gmail.com>
2024-07-30 20:47:57 +0100

    Use in memory user details manager to add new user

--- src/main/java/com/udemy/learn/spring/security/config/BasicAuthSecurityConfiguration.java
+++ src/main/java/com/udemy/learn/spring/security/config/BasicAuthSecurityConfiguration.java
@@ -6,12 +6,47 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.function.Function;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 public class BasicAuthSecurityConfiguration{
+
+
+    @Bean
+    public InMemoryUserDetailsManager createUserDetailsManager() {
+
+        UserDetails userDetails1 = createNewUser("priti", "priti");
+        UserDetails userDetails2 = createNewUser("harkiran", "password");
+
+        return new InMemoryUserDetailsManager(userDetails1, userDetails2);
+    }
+
+    private UserDetails createNewUser(String username, String password) {
+        Function<String, String> passwordEncoder
+                = input -> passwordEncoder().encode(input);
+
+        UserDetails userDetails = User.builder()
+                .passwordEncoder(passwordEncoder)
+                .username(username)
+                .password(password)
+                .roles("USER","ADMIN")
+                .build();
+        return userDetails;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
